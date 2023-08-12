886afa0554016bf71e811cd0a1bd4171a9c7407f
probalazs <probalazs@nyenyere.eu>
2023-08-12 14:51:47 +0200

    Fix menu icon.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi'
+  },
   components,
   directives
 })
